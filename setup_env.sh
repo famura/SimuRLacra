94a075eaf3cb3e80184f2e9e3f75fd11fdf2596b
Fabio Muratore <fabio.muratore@famura.net>
2020-08-20 17:53:22 +0200

    Reworked setup process

--- setup_env.sh
+++ setup_env.sh
@@ -32,8 +32,8 @@ conda init
 
 source ~/.bashrc
 
-conda create -n pyrado python=3.7 blas cmake colorama coverage cython joblib lapack libgcc-ng mkl matplotlib-base numpy pandas patchelf pip pycairo pytest pytest-cov pytest-xdist pyyaml scipy seaborn setuptools sphinx sphinx-math-dollar sphinx_rtd_theme tabulate tqdm -c conda-forge
+conda create -n pyrado python=3.7 blas cmake colorama coverage cython joblib lapack libgcc-ng mkl matplotlib-base numpy optuna pandas patchelf pip pycairo pytest pytest-cov pytest-xdist pyyaml scipy seaborn setuptools sphinx sphinx-math-dollar sphinx_rtd_theme tabulate tqdm -c conda-forge
 
 conda activate pyrado
 
-conda run -n pyrado /bin/bash -c "pip install git+https://github.com/Xfel/init-args-serializer.git@master argparse box2d glfw gym pprint pytest-lazy-fixture vpython"
+conda run -n pyrado /bin/bash -c "pip install git+https://github.com/Xfel/init-args-serializer.git@master argparse box2d glfw gym prettyprinter pytest-lazy-fixture vpython"
