98b2327cfdc9bebe04177cda09a7da4956c8dab0
Fabio Muratore <fabio.muratore@famura.net>
2020-08-26 08:32:51 +0200

    Added test_algorithms.py

---
+++ Pyrado/tests/environment_wrappers/test_combination.py
@@ -0,0 +1,97 @@
+# Copyright (c) 2020, Fabio Muratore, Honda Research Institute Europe GmbH, and
+# Technical University of Darmstadt.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of Fabio Muratore, Honda Research Institute Europe GmbH,
+#    or Technical University of Darmstadt, nor the names of its contributors may
+#    be used to endorse or promote products derived from this software without
+#    specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL FABIO MURATORE, HONDA RESEARCH INSTITUTE EUROPE GMBH,
+# OR TECHNICAL UNIVERSITY OF DARMSTADT BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import numpy as np
+import pytest
+
+from pyrado.domain_randomization.default_randomizers import get_default_randomizer
+from pyrado.environment_wrappers.action_delay import ActDelayWrapper
+from pyrado.environment_wrappers.action_noise import GaussianActNoiseWrapper
+from pyrado.environment_wrappers.action_normalization import ActNormWrapper
+from pyrado.environment_wrappers.domain_randomization import DomainRandWrapperBuffer
+from pyrado.environment_wrappers.observation_normalization import ObsNormWrapper
+from pyrado.environment_wrappers.observation_partial import ObsPartialWrapper
+from pyrado.environment_wrappers.utils import inner_env, remove_env, typed_env
+from pyrado.environments.pysim.quanser_cartpole import QCartPoleSwingUpSim
+from pyrado.policies.dummy import DummyPolicy
+from pyrado.sampling.rollout import rollout
+from pyrado.utils.data_types import RenderMode
+
+
+@pytest.mark.wrapper
+def test_combination():
+    env = QCartPoleSwingUpSim(dt=1/50., max_steps=20)
+
+    randomizer = get_default_randomizer(env)
+    env_r = DomainRandWrapperBuffer(env, randomizer)
+    env_r.fill_buffer(num_domains=3)
+
+    dp_before = []
+    dp_after = []
+    for i in range(4):
+        dp_before.append(env_r.domain_param)
+        rollout(env_r, DummyPolicy(env_r.spec), eval=True, seed=0, render_mode=RenderMode())
+        dp_after.append(env_r.domain_param)
+        assert dp_after[i] != dp_before[i]
+    assert dp_after[0] == dp_after[3]
+
+    env_rn = ActNormWrapper(env)
+    elb = {r'$\dot{x}$': -213., r'$\dot{\theta}$': -42.}
+    eub = {r'$\dot{x}$': 213., r'$\dot{\theta}$': 42., r'$x$': 0.123}
+    env_rn = ObsNormWrapper(env_rn, explicit_lb=elb, explicit_ub=eub)
+    alb, aub = env_rn.act_space.bounds
+    assert all(alb == -1)
+    assert all(aub == 1)
+    olb, oub = env_rn.obs_space.bounds
+    assert all(olb == -1)
+    assert all(oub == 1)
+
+    ro_r = rollout(env_r, DummyPolicy(env_r.spec), eval=True, seed=0, render_mode=RenderMode())
+    ro_rn = rollout(env_rn, DummyPolicy(env_rn.spec), eval=True, seed=0, render_mode=RenderMode())
+    assert np.allclose(env_rn._process_obs(ro_r.observations), ro_rn.observations)
+
+    env_rnp = ObsPartialWrapper(env_rn, idcs=[r'$\dot{x}$', r'$\cos(\theta)$'])
+    ro_rnp = rollout(env_rnp, DummyPolicy(env_rnp.spec), eval=True, seed=0, render_mode=RenderMode())
+
+    env_rnpa = GaussianActNoiseWrapper(env_rnp,
+                                       noise_mean=0.5*np.ones(env_rnp.act_space.shape),
+                                       noise_std=0.1*np.ones(env_rnp.act_space.shape))
+    ro_rnpa = rollout(env_rnpa, DummyPolicy(env_rnpa.spec), eval=True, seed=0, render_mode=RenderMode())
+    assert np.allclose(ro_rnp.actions, ro_rnpa.actions)
+    assert not np.allclose(ro_rnp.observations, ro_rnpa.observations)
+
+    env_rnpd = ActDelayWrapper(env_rnp, delay=3)
+    ro_rnpd = rollout(env_rnpd, DummyPolicy(env_rnpd.spec), eval=True, seed=0, render_mode=RenderMode())
+    assert np.allclose(ro_rnp.actions, ro_rnpd.actions)
+    assert not np.allclose(ro_rnp.observations, ro_rnpd.observations)
+
+    assert isinstance(inner_env(env_rnpd), QCartPoleSwingUpSim)
+    assert typed_env(env_rnpd, ObsPartialWrapper) is not None
+    assert isinstance(env_rnpd, ActDelayWrapper)
+    env_rnpdr = remove_env(env_rnpd, ActDelayWrapper)
+    assert not isinstance(env_rnpdr, ActDelayWrapper)
