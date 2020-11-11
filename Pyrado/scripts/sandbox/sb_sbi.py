1680955be6aff4a995435c1fc379d07c0c8f8ecf
Fabio Muratore <fabio.muratore@famura.net>
2020-11-11 13:10:39 +0100

    Using the omo environment for the sandbox script

--- Pyrado/scripts/sandbox/sb_sbi.py
+++ Pyrado/scripts/sandbox/sb_sbi.py
@@ -35,26 +35,41 @@ Testing the simulation-based inference (SBI) toolbox
 import numpy as np
 import sbi.utils as utils
 import seaborn as sns
-import torch
+import torch as to
 from matplotlib import pyplot as plt
 from sbi.inference.base import infer
+
+import pyrado
+from pyrado.environments.pysim.one_mass_oscillator import OneMassOscillatorSim
+from pyrado.policies.dummy import IdlePolicy
+from pyrado.sampling.rollout import rollout
+
 
 plt.rcParams.update({"text.usetex": True})
 
 
 def simulator(mu):
-    # Generate samples from N(mu, sigma=0.5)
-    return mu + 0.5 * torch.randn_like(mu)
+    # In the end, the output of this could be a distance measure over trajectories instead of just the final state
+    ro = rollout(env, policy, eval=True, reset_kwargs=dict(
+        # domain_param=dict(k=mu[0], d=mu[1]), init_state=np.array([-0.7, 0.])  # no variance over the init state
+        domain_param=dict(k=mu[0], d=mu[1])  # no variance over the parameters
+    ))
+    return to.from_numpy(ro.observations[-1]).to(dtype=to.float32)
 
 
 if __name__ == '__main__':
+    pyrado.set_seed(0)
+
+    env = OneMassOscillatorSim(dt=0.005, max_steps=200)
+    policy = IdlePolicy(env.spec)
+
     prior = utils.BoxUniform(
-        low=torch.tensor([-5., -5.]),
-        high=torch.tensor([5., 5.])
+        low=to.tensor([25., 0.05]),
+        high=to.tensor([35., 0.15])
     )
 
     # Let’s learn a likelihood from the simulator
-    num_sim = 200
+    num_sim = 500
     method = 'SNRE'  # SNPE or SNLE or SNRE
     posterior = infer(
         simulator,
@@ -63,17 +78,20 @@ if __name__ == '__main__':
         num_workers=-1,
         num_simulations=num_sim)
 
-    # Let’s record our 5 “observations” of the true distribution
+    # Let’s record our “observations” of the true distribution
     n_observations = 5
-    observation = torch.tensor([3., -1.5])[None] + 0.5*torch.randn(n_observations, 2)
+    # noisy_true_params = to.tensor([30, 0.1]) + to.tensor([30, 0.1])*to.randn(n_observations, 2)/10  # no variance over the init state
+    noisy_true_params = to.tensor([30, 0.1]).repeat((n_observations, 1))  # no variance over the parameters
+    observation = to.stack([simulator(dp) for dp in noisy_true_params])
 
     # Inference
-    samples = posterior.sample((200,), x=observation[0])  # sample the posterior for a single data point
+    # samples = posterior.sample((200,), x=observation[0])  # sample the posterior for a single data point
+    samples = to.cat([posterior.sample((200,), x=obs) for obs in observation], dim=0)
 
     # Computing the log-probability
-    bounds = [3-1, 3+1, -1.5-1, -1.5+1]
-    mu_1, mu_2 = torch.tensor(np.mgrid[bounds[0]:bounds[1]:2/50., bounds[2]:bounds[3]:2/50.]).float()
-    grids = torch.cat(
+    bounds = [20, 40, 0.0, 0.2]
+    mu_1, mu_2 = to.tensor(np.mgrid[bounds[0]:bounds[1]:20/50., bounds[2]:bounds[3]:0.2/50.]).float()
+    grids = to.cat(
         (mu_1.reshape(-1, 1), mu_2.reshape(-1, 1)),
         dim=1
     )
@@ -84,47 +102,27 @@ if __name__ == '__main__':
         ])
     else:
         log_prob = sum([
-            posterior.net(torch.cat((grids, observation[i].repeat((grids.shape[0])).reshape(-1, 2)), dim=1))[:, 0] +
+            posterior.net(to.cat([grids, observation[i].repeat((grids.shape[0], 1))], dim=1))[:, 0] +
             posterior._prior.log_prob(grids)
             for i in range(len(observation))
         ]).detach()
-    prob = torch.exp(log_prob - log_prob.max())  # scale the probabilities to [0, 1]
+    prob = to.exp(log_prob - log_prob.max())  # scale the probabilities to [0, 1]
 
-    log_probability = posterior.log_prob(samples, x=observation[0])
-    out = utils.pairplot(samples, limits=[[-5,5],[-5,5]], fig_size=(6,6), upper='kde', diag='kde')
-
-    true_like = lambda x: -((x[0] - mu_1)**2 + (x[1] - mu_2)**2)/(2*0.5**2)
-    true_log_prob = sum([true_like(observation[i]) for i in range(len(observation))])
+    # log_probability = posterior.log_prob(samples, x=observation[0])  # log_probability seems to be unused
 
     # Plot
     sns.scatterplot(x=observation[:, 0], y=observation[:, 1])
-    plt.xlabel(r'$$x_1$$')
-    plt.ylabel(r'$$x_2$$')
-    plt.xlim(-10, 10)
-    plt.ylim(-10, 10)
+    plt.xlabel(r'$x_0$')
+    plt.ylabel(r'$x_1$')
+
+    out = utils.pairplot(samples, limits=[[20., 40.], [0.0, 0.2]], fig_size=(6, 6), upper='kde', diag='kde')
 
     plt.figure(dpi=200)
-    plt.plot([2, 4], [-1.5, -1.5], color='k')
-    plt.plot([3, 3], [-0.5, -2.5], color='k')
-    plt.contourf(prob.reshape(*mu_1.shape), extent=bounds, origin='lower')
-    plt.axis('scaled')
-    plt.xlim(2+0.3, 4-0.3)
-    plt.ylim(-2.5+0.3, -0.5-0.3)
-    plt.title(r'Posterior with learned likelihood\nfrom %d examples of'%(num_sim)+r' $$\mu_i\in[-5, 5]$$')
-    plt.xlabel(r'$$\mu_1$$')
-    plt.ylabel(r'$$\mu_2$$')
-
-    plt.figure(dpi=200)
-    prob = torch.exp(true_log_prob - true_log_prob.max())
-    plt.plot([2, 4], [-1.5, -1.5], color='k')
-    plt.plot([3, 3], [-0.5, -2.5], color='k')
-    plt.contourf(prob.reshape(*mu_1.shape), extent=bounds, origin='lower')
-    plt.axis('scaled')
-    plt.xlim(2+0.3, 4-0.3)
-    plt.ylim(-2.5+0.3, -0.5-0.3)
-    plt.title('Posterior with\nanalytic likelihood')
-    plt.xlabel(r'$$\mu_1$$')
-    plt.ylabel(r'$$\mu_2$$')
-
+    plt.plot([20., 40.], [0.1, 0.1], color='w', ls='--')
+    plt.plot([30., 30.], [0.0, 0.2], color='w', ls='--')
+    plt.contourf(mu_1.numpy(), mu_2.numpy(), prob.reshape(*mu_1.shape).numpy())
+    # plt.contourf(prob.reshape(*mu_1.shape), extent=bounds, origin='lower')
+    plt.title(f'Posterior with learned likelihood \n from {num_sim} examples of the true distribution')
+    plt.xlabel(r'$k$')
+    plt.ylabel(r'$d$')
     plt.show()
-
