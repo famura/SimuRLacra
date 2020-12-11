515d85ab7b9193d202c8472eb981385d3e8c3c14
Fabio Muratore <fabio.muratore@famura.net>
2020-12-11 14:57:40 +0100

    Made license more visible

--- RcsPySim/setup.py
+++ RcsPySim/setup.py
@@ -38,7 +38,7 @@ from distutils.version import LooseVersion
 
 
 class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=''):
+    def __init__(self, name, sourcedir=""):
         Extension.__init__(self, name, sources=[])
         self.sourcedir = os.path.abspath(sourcedir)
 
@@ -46,14 +46,16 @@ class CMakeExtension(Extension):
 class CMakeBuild(build_ext):
     def run(self):
         try:
-            out = subprocess.check_output(['cmake', '--version'])
+            out = subprocess.check_output(["cmake", "--version"])
         except OSError:
-            raise RuntimeError("CMake must be installed to build the following extensions: " +
-                               ", ".join(e.name for e in self.extensions))
+            raise RuntimeError(
+                "CMake must be installed to build the following extensions: "
+                + ", ".join(e.name for e in self.extensions)
+            )
 
         if platform.system() == "Windows":
-            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
-            if cmake_version < '3.1.0':
+            cmake_version = LooseVersion(re.search(r"version\s*([\d.]+)", out.decode()).group(1))
+            if cmake_version < "3.1.0":
                 raise RuntimeError("CMake >= 3.1.0 is required on Windows")
 
         for ext in self.extensions:
@@ -63,49 +65,47 @@ class CMakeBuild(build_ext):
         if isinstance(ext, CMakeExtension):
             # Build a cmake extension
             extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-            cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                          '-DPYTHON_EXECUTABLE=' + sys.executable]
+            cmake_args = ["-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir, "-DPYTHON_EXECUTABLE=" + sys.executable]
 
-            cfg = 'Debug' if self.debug else 'Release'
-            build_args = ['--config', cfg]
-            build_args = ['--target', ext.name]
+            cfg = "Debug" if self.debug else "Release"
+            build_args = ["--config", cfg]
+            build_args = ["--target", ext.name]
 
             if platform.system() == "Windows":
-                cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
-                if sys.maxsize > 2**32:
-                    cmake_args += ['-A', 'x64']
-                build_args += ['--', '/m']
+                cmake_args += ["-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)]
+                if sys.maxsize > 2 ** 32:
+                    cmake_args += ["-A", "x64"]
+                build_args += ["--", "/m"]
             else:
-                cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-                build_args += ['--', '-j2']
+                cmake_args += ["-DCMAKE_BUILD_TYPE=" + cfg]
+                build_args += ["--", "-j2"]
 
             if not os.path.exists(self.build_temp):
                 os.makedirs(self.build_temp)
-            subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp)
-            subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+            subprocess.check_call(["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp)
+            subprocess.check_call(["cmake", "--build", "."] + build_args, cwd=self.build_temp)
         else:
             super().build_extension(ext)
 
 
 setup(
-    name='RcsPySim',
-    version='0.2',
-    description='Robotic simulation environments backed by Rcs',
-    author='Fabio Muratore & Felix Treede',
-    author_email='fabio.muratore@famura.net',
+    name="RcsPySim",
+    version="0.2",
+    description="Robotic simulation environments backed by Rcs",
+    author="Fabio Muratore & Felix Treede",
+    author_email="fabio.muratore@famura.net",
+    license="BSD 3-clause",
     # Specify empty py_modules to exclude pkgConfig.py
     py_modules=[],
-    packages=['rcsenv'],
+    packages=["rcsenv"],
     # Source directory
-    package_dir={'': 'src/python/'},
+    package_dir={"": "src/python/"},
     # Include config files
-    package_data={'rcsenv': ['config/**/*']},
+    package_data={"rcsenv": ["config/**/*"]},
     include_package_data=True,
     # Build CMake extensions
-    ext_modules=[CMakeExtension('_rcsenv')],
+    ext_modules=[CMakeExtension("_rcsenv")],
     cmdclass=dict(build_ext=CMakeBuild),
     # Dependencies
-    install_requires=[
-        'numpy'
-    ],
+    install_requires=["numpy"],
 )
