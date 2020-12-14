50d36704011ef73646281dc0ce9d1501e4771c48
theo <theo.gruner@gmail.com>
2020-12-14 21:56:00 +0100

    split lfi_comparison into scprit file utilities

---
+++ Pyrado/scripts/lfi/lfi_omo_snpe.py
@@ -0,0 +1,77 @@
+import numpy as np
+import sbi.utils as utils
+import seaborn as sns
+import torch as to
+import torch.nn as nn
+from matplotlib import pyplot as plt
+from sbi.inference.base import infer
+from sbi.inference import SNPE, SNPE_B, SNPE_C, SNLE, prepare_for_sbi, simulate_for_sbi
+
+import pyrado
+from pyrado.environments.pysim.one_mass_oscillator import OneMassOscillatorSim
+from pyrado.policies.special.dummy import IdlePolicy
+from pyrado.sampling.rollout import rollout
+from scripts.lfi.plot_trajectories import plot_trajectories
+from scripts.lfi.train_lfi import *
+
+
+def fancy_simulator(mu):
+    # In the end, the output of this could be a distance measure over trajectories instead of just the final state
+    ro = rollout(env, policy, eval=True, reset_kwargs=dict(
+        domain_param=dict(k=mu[0], d=mu[1])
+    ))
+    return to.tensor(ro.observations).to(dtype=to.float32).view(-1, 1).squeeze()
+
+
+if __name__ == '__main__':
+    num_sim = 5
+    num_rounds = 1
+    num_samples = 200
+    n_observations = 5
+
+    # define simulator
+    env = OneMassOscillatorSim(dt=0.005, max_steps=200)
+    policy = IdlePolicy(env.spec)
+    simulator = fancy_simulator
+    prior = utils.BoxUniform(
+        low=to.tensor([25., 0.05]),
+        high=to.tensor([35., 0.15])
+    )
+
+    # real environment
+    true_params = to.tensor([30, 0.1]).to(dtype=to.float32)
+    x_o = simulator(true_params)
+
+    # define algorithm hyperparameters
+    input_length = len(x_o)
+    embedding_net = nn.Linear(input_length, 10).to(dtype=to.float32)
+    simulator, prior = prepare_for_sbi(simulator, prior)
+    model = utils.posterior_nn(model='maf', hidden_features=10,
+                               num_transforms=2, embedding_net=embedding_net)
+
+    inference = SNPE(prior, density_estimator=model)
+
+    # train the posterior
+    posterior = train_lfi(simulator, inference, prior, x_o, num_sim=10, num_rounds=2)
+
+    # generate examples from the posterior
+    proposals, log_prob, trajectories = evaluate_lfi(simulator=simulator, posterior=posterior, x_o=x_o)
+
+    # save model
+    save_nn(posterior.net, "model_snpe")
+
+    # testing to load the model
+    # new_model = utils.posterior_nn(model='maf', hidden_features=10,
+    #                                num_transforms=2, embedding_net=embedding_net)
+    # new_inference = SNPE(prior, density_estimator=new_model)
+    # new_posterior = train_lfi(simulator, inference, prior, x_o, num_sim=1, num_rounds=1)
+
+    # TODO: come up with smarter way to load to initialize the posterior
+    # new_posterior = DirectPosterior(method_family="snpe",
+    #                                 neural_net=model(true_params.unsqueeze(0), x_o.unsqueeze(0)),
+    #                                 prior=prior,
+    #                                 x_shape=x_o.shape)
+
+    # load_nn(new_posterior.net, "sbi_logs")
+
+    # plot useful statistics
---
+++ Pyrado/scripts/lfi/train_lfi.py
@@ -0,0 +1,48 @@
+from collections import Callable
+import torch as to
+from torch.utils.tensorboard import SummaryWriter
+
+from sbi.inference import simulate_for_sbi
+from sbi.inference.posteriors.direct_posterior import DirectPosterior
+
+
+def train_lfi(simulator,
+              inference,
+              prior,
+              x_o,
+              num_rounds: int = 5,
+              num_sim: int = 1000,
+              summary: SummaryWriter = None
+              ) -> DirectPosterior:
+    proposal_prior = prior
+    for _ in range(num_rounds):
+        theta, x = simulate_for_sbi(simulator, proposal_prior, num_simulations=num_sim, simulation_batch_size=1)
+        _ = inference.append_simulations(theta, x).train()
+        proposal_prior = inference.build_posterior().set_default_x(x_o)
+    posterior = proposal_prior
+    return posterior
+
+
+def evaluate_lfi(simulator: Callable,
+                 posterior: DirectPosterior,
+                 x_o,
+                 num_samples: int = 1000):
+
+    proposals = posterior.sample((num_samples, ), x=x_o)
+    log_prob = posterior.log_prob(proposals, x=x_o)
+    trajectories = []
+    for proposal in proposals:
+        trajectory = simulator(proposal.unsqueeze(0))
+        trajectories.append(trajectory)
+    return proposals, log_prob, trajectories
+
+
+def save_nn(nn: to.nn.Module, path: str):
+    to.save(nn.state_dict(), path)
+    print("saved model at:\t {}".format(path))
+
+
+def load_nn(nn, path: str):
+    state_dict = to.load(path)
+    nn.load_state_dict(state_dict=state_dict)
+    print("loaded model from: {}".format(path))
