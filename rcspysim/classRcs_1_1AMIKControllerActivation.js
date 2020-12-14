ad00513653625c45ffe1aa037f3023e26af227cb
famura <37794142+famura@users.noreply.github.com>
2020-12-14 18:22:50 +0000

    Deploying to gh-pages from  @ 4605b3e2aeae139766afaa097d877208c9cf457b 🚀

--- rcspysim/classRcs_1_1AMIKControllerActivation.js
+++ rcspysim/classRcs_1_1AMIKControllerActivation.js
@@ -2,6 +2,7 @@ var classRcs_1_1AMIKControllerActivation =
 [
     [ "AMIKControllerActivation", "classRcs_1_1AMIKControllerActivation.html#a5bffdd02c7f1f2ed8ea5bbf133b689f1", null ],
     [ "~AMIKControllerActivation", "classRcs_1_1AMIKControllerActivation.html#a08c9352fed10c0babbe729b0ac17a9fd", null ],
+    [ "addAlwaysActiveTask", "classRcs_1_1AMIKControllerActivation.html#a23efd0ebdb319ce0f7c040a008d07928", null ],
     [ "checkTaskCombinationMethod", "classRcs_1_1AMIKControllerActivation.html#afa3ccd9b469a76436eaa15d22dd38862", null ],
     [ "computeCommand", "classRcs_1_1AMIKControllerActivation.html#ad0b13f5b3c58aa714f4198ff7a4bc7bd", null ],
     [ "getActivation", "classRcs_1_1AMIKControllerActivation.html#acd1a8f8b48e0b44d1dda9073e08fc3f2", null ],
@@ -13,8 +14,9 @@ var classRcs_1_1AMIKControllerActivation =
     [ "getXdes", "classRcs_1_1AMIKControllerActivation.html#ae14f1742c33875de37006c7f0862e6f5", null ],
     [ "reset", "classRcs_1_1AMIKControllerActivation.html#a7c03c520431fc803007aaa80c6c3c850", null ],
     [ "setXdes", "classRcs_1_1AMIKControllerActivation.html#abcbf12e106b9697e6fd7ba1ee9573cb0", null ],
-    [ "setXdesFromTaskSpec", "classRcs_1_1AMIKControllerActivation.html#ae065fbc3862594858f64afd336d04720", null ],
+    [ "setXdesFromTaskSpec", "classRcs_1_1AMIKControllerActivation.html#a0d0b86614ebaf28589decdd03d980ef5", null ],
     [ "activation", "classRcs_1_1AMIKControllerActivation.html#a23781dd23bb6fafd72a02dbb1dc4c39c", null ],
+    [ "dimAlwaysActiveTasks", "classRcs_1_1AMIKControllerActivation.html#aea158b2f8334e5547c6216f6321b5ab9", null ],
     [ "taskCombinationMethod", "classRcs_1_1AMIKControllerActivation.html#ad2d9ffe397b29de6a5391b95f28582cc", null ],
     [ "x_des", "classRcs_1_1AMIKControllerActivation.html#aab86f67c93e1d0134035128cd74d0e3e", null ]
 ];
