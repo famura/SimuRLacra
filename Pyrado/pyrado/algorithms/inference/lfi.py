2a5fdd25f3500c04def255232bfd92ee75df8884
Theo <theo.gruner@gmail.com>
2021-01-06 14:50:32 +0100

    added logging during training

--- Pyrado/pyrado/algorithms/inference/lfi.py
+++ Pyrado/pyrado/algorithms/inference/lfi.py
@@ -23,10 +23,10 @@ class EnvSimulator(Callable):
     """
 
     def __init__(
-        self,
-        env: Env,
-        policy: Policy,
-        param_names: list,
+            self,
+            env: Env,
+            policy: Policy,
+            param_names: list,
     ):
         self.name = env.name
         self.env = env
@@ -52,18 +52,19 @@ class LFI(LoggerAware):
     """
 
     def __init__(
-        self,
-        save_dir: str,
-        simulator: Callable,
-        params_names,
-        prior: Distribution,
-        inference: Type[PosteriorEstimator] = None,
-        flow: Callable[[], DirectPosterior] = None,
-        posterior: DirectPosterior = None,
-        max_iter: int = 5,
-        num_sim: int = 1000,
-        logger: Optional[StepLogger] = None,
-        save_name: str = "algo",
+            self,
+            save_dir: str,
+            simulator: Callable,
+            params_names,
+            prior: Distribution,
+            inference: Type[PosteriorEstimator] = None,
+            flow: Callable[[], DirectPosterior] = None,
+            posterior: DirectPosterior = None,
+            max_iter: int = 5,
+            num_sim: int = 5,
+            num_samples=25,
+            logger: Optional[StepLogger] = None,
+            save_name: str = "algo",
     ):
         self._save_dir = save_dir
         self.posterior = posterior
@@ -75,6 +76,7 @@ class LFI(LoggerAware):
         self._max_iter = max_iter
         self._curr_iter = 0
         self._num_sim = num_sim
+        self._num_samples = num_samples
         self._cnt_samples = 0
         self.batch_simulator, self.batch_prior = prepare_for_sbi(self.simulator, self.prior)
 
@@ -99,7 +101,7 @@ class LFI(LoggerAware):
         """
         self.posterior = posterior
 
-    def step(self, snapshot_mode: str, meta_info: dict = None):
+    def step(self, snapshot_mode: str, meta_info: dict = None, logging=False):
         """
         Trains the posterior using SNPE using observed rollouts and the prior distribution
 
@@ -108,72 +110,121 @@ class LFI(LoggerAware):
         #  and should only be used for evaluation
 
         # get real-world rollouts from meta_info
+        if "rollouts_real" not in meta_info.keys():
+            raise pyrado.KeyErr(keys="rollouts_real")
         rollouts_real = meta_info["rollouts_real"]
+        if rollouts_real.dim() == 1:
+            # add a dimension to treat single rollouts as a batch of data
+            rollouts_real = rollouts_real.unsqueeze(0)
+        if rollouts_real.dim() > 2:
+            raise pyrado.ShapeErr(given=rollouts_real)
+
+        n_sim = 0
+        if logging:
+            log_probs = []
+            n_simulations = []
 
         # set proposal prior
         proposal_prior = self.batch_prior
+
+        # first iteration
         if self._curr_iter == 0:
             theta, x = simulate_for_sbi(
                 self.batch_simulator, proposal_prior, num_simulations=self._num_sim, simulation_batch_size=1
             )
             _ = self.inference.append_simulations(theta, x).train()
-            proposal_prior = self.inference.build_posterior()
+            self.posterior = self.inference.build_posterior()
+
+            if logging:
+                _, log_prob, _ = self.evaluate(rollouts_real=rollouts_real,
+                                               num_samples=self._num_samples,
+                                               compute_quantity={"log_prob": True})
+                log_prob = log_prob.mean().squeeze()
+                n_sim += self._num_sim
+                n_simulations.append(n_sim)
+                log_probs.append(log_prob)
+                self.logger.add_value("Mean Log Probability", log_prob)
+                self.logger.add_value("Number of Simulations", to.tensor(n_sim))
+
             self._curr_iter += 1
+
+            self.logger.add_value("Current Iteration", self._curr_iter)
+
+        # remaining training steps
         while self._curr_iter < self._max_iter:
-            # generate data to train
-            if len(rollouts_real) == 1:
+            for ro in rollouts_real:
+                self.posterior.set_default_x(ro)
                 theta, x = simulate_for_sbi(
-                    self.batch_simulator, proposal_prior, num_simulations=self._num_sim, simulation_batch_size=1
+                    self.batch_simulator, self.posterior, num_simulations=self._num_sim, simulation_batch_size=1
                 )
                 self.inference.append_simulations(theta, x)
-            else:
-                for ro in rollouts_real:
-                    proposal_prior.set_default_x(ro)
-                    theta, x = simulate_for_sbi(
-                        self.batch_simulator, proposal_prior, num_simulations=self._num_sim, simulation_batch_size=1
-                    )
-                    self.inference.append_simulations(theta, x)
+                n_sim += self._num_sim
 
             _ = self.inference.train()
             # set proposal prior
-            proposal_prior = self.inference.build_posterior()
+            self.posterior = self.inference.build_posterior()
+
+            if logging:
+                _, log_prob, _ = self.evaluate(rollouts_real=rollouts_real,
+                                               num_samples=self._num_samples,
+                                               compute_quantity={"log_prob": True})
+                log_prob = log_prob.mean().squeeze()
+                log_probs.append(log_prob)
+                n_simulations.append(n_sim)
+                self.logger.add_value("Mean Log Probability", log_prob)
+                self.logger.add_value("Number of Simulations", to.tensor(n_sim))
+
+                meta_info = {**meta_info,
+                             **dict(zip(["log_probs", "n_simulations"], [to.stack(log_probs),
+                                                                         to.tensor(n_simulations)]))}
+
             self._curr_iter += 1
             self.logger.add_value("Current Iteration", self._curr_iter)
-        self.posterior = proposal_prior
-        self.make_snapshot(snapshot_mode=snapshot_mode, meta_info=meta_info)
+            self.make_snapshot(snapshot_mode=snapshot_mode, meta_info=meta_info)
 
     def evaluate(
-        self,
-        obs_traj: to.Tensor,
-        num_samples: int = 1000,
-        compute_quantity: dict = None,
+            self,
+            rollouts_real: to.Tensor,
+            num_samples: int = 1000,
+            compute_quantity: dict = None,
     ):
         """
         Evaluates the posterior by calculating parameter samples given observed data, its log probability
         and the simulated trajectory.
         """
-        # default computations
         compute_dict = {"log_prob": False, "sample_params": False, "sim_traj": False}
         if compute_quantity is not None:
-            # TODO: raise exception if keys do not fit given quantities
+            if not all(k in compute_dict for k in compute_quantity):
+                raise pyrado.KeyErr(keys=list(compute_quantity.keys()))
             compute_dict.update(compute_quantity)
 
+        if rollouts_real.dim() == 1:
+            # add a dimension to treat single rollouts as a batch of data
+            rollouts_real = rollouts_real.unsqueeze(0)
+        if rollouts_real.dim() > 2:
+            raise pyrado.ShapeErr(given=rollouts_real)
+
         # generate sample parameters
-        prop_params = to.stack([self.posterior.sample((num_samples,), x=obs) for obs in obs_traj], dim=0)
+        prop_params = to.stack([self.posterior.sample((num_samples,), x=obs) for obs in rollouts_real], dim=0)
+        # prop_params = [self.posterior.sample((num_samples,), x=obs) for obs in rollouts_real]
 
         log_prob, sim_traj = None, None
-        num_obs = obs_traj.shape[0]
-        len_obs = obs_traj.shape[1]
+        num_obs = rollouts_real.shape[0]
+        len_obs = rollouts_real.shape[1]
         if compute_dict["sim_traj"]:
+            # sim_trajs = []
             sim_traj = to.empty((num_obs, num_samples, len_obs))
         if compute_dict["log_prob"]:
+            # log_probs = []
             log_prob = to.empty((num_obs, num_samples))
 
         cnt = 0
         for o in range(num_obs):
+
             # compute log probability
             if compute_dict["log_prob"]:
-                log_prob[o, :] = self.posterior.log_prob(prop_params[o, :, :], x=obs_traj[o, :])
+                log_prob[o, :] = self.posterior.log_prob(prop_params[o, :, :], x=rollouts_real[o, :])
+                # log_probs.append(self.posterior.log_prob(prop_params[o], x=rollouts_real[o]))
 
             # compute trajectories
             if compute_dict["sim_traj"]:
@@ -186,7 +237,8 @@ class LFI(LoggerAware):
                             end="",
                         )
                     # compute trajectories for each observation and every sample
-                    sim_traj[o, s, :] = self.simulator(prop_params[o, s, :].unsqueeze(0))
+                    sim_traj[o, s, :] = self.batch_simulator(prop_params[o, s, :].unsqueeze(0))
+                    # sim_trajs.append(self.batch_simulator(prop_params[o]))
             cnt += 1
         if not compute_dict["sample_params"]:
             prop_params = None
@@ -215,3 +267,7 @@ class LFI(LoggerAware):
         if "rollouts_real" not in meta_info:
             raise pyrado.KeyErr(keys="rollouts_real", container=meta_info)
         pyrado.save(meta_info["rollouts_real"], "rollouts_real", "pkl", self._save_dir, meta_info)
+
+        if "log_probs" in meta_info:
+            pyrado.save(meta_info["log_probs"], "log_probs", "pkl", self._save_dir)
+            pyrado.save(meta_info["n_simulations"], "n_simulations", "pkl", self._save_dir)
