d4cae4711b220374a368ebc367e617c018595ce8
Heiko Carrasco <heiko.carrasco@yahoo.com>
2021-02-01 19:03:12 +0100

    Adapted to new save_dicts_to_yaml signature

--- Pyrado/scripts/training/qq-su_ppo_sprl.py
+++ Pyrado/scripts/training/qq-su_ppo_sprl.py
@@ -39,7 +39,7 @@ from pyrado.domain_randomization.domain_randomizer import DomainRandomizer
 from pyrado.environment_wrappers.action_normalization import ActNormWrapper
 from pyrado.environment_wrappers.domain_randomization import DomainRandWrapperLive
 from pyrado.environments.pysim.quanser_qube import QQubeSwingUpSim
-from pyrado.logger.experiment import setup_experiment, save_list_of_dicts_to_yaml
+from pyrado.logger.experiment import setup_experiment, save_dicts_to_yaml
 from pyrado.policies.feed_forward.fnn import FNNPolicy
 from pyrado.spaces import ValueFunctionSpace
 from pyrado.utils.argparser import get_argparser
@@ -122,15 +122,13 @@ if __name__ == "__main__":
     algo = SPRL(env, PPO(ex_dir, env, policy, critic, **algo_hparam), **sprl_hparam)
 
     # Save the hyper-parameters
-    save_list_of_dicts_to_yaml(
-        [
-            dict(env=env_hparams, seed=args.seed),
-            dict(policy=policy_hparam),
-            dict(critic=critic_hparam, vfcn=vfcn_hparam),
-            dict(subrtn=algo_hparam, subrtn_name=PPO.name),
-            dict(algo=sprl_hparam, algo_name=algo.name),
-        ],
-        ex_dir,
+    save_dicts_to_yaml(
+        dict(env=env_hparams, seed=args.seed),
+        dict(policy=policy_hparam),
+        dict(critic=critic_hparam, vfcn=vfcn_hparam),
+        dict(subrtn=algo_hparam, subrtn_name=PPO.name),
+        dict(algo=sprl_hparam, algo_name=algo.name),
+        save_dir=ex_dir,
     )
 
     # Jeeeha
