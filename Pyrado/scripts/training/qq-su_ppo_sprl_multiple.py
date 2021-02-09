419f77c782c9dea896083a1e2c44097f8b38bbec
Heiko Carrasco <heiko.carrasco@yahoo.com>
2021-02-09 19:43:54 +0100

    Add multiple domain param test

---
+++ Pyrado/scripts/training/qq-su_ppo_sprl_multiple.py
@@ -0,0 +1,149 @@
+# Copyright (c) 2020, Fabio Muratore, Honda Research Institute Europe GmbH, and
+# Technical University of Darmstadt.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of Fabio Muratore, Honda Research Institute Europe GmbH,
+#    or Technical University of Darmstadt, nor the names of its contributors may
+#    be used to endorse or promote products derived from this software without
+#    specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL FABIO MURATORE, HONDA RESEARCH INSTITUTE EUROPE GMBH,
+# OR TECHNICAL UNIVERSITY OF DARMSTADT BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+"""
+Train an agent to solve the Quanser Qube swing-up task using Proximal Policy Optimization.
+"""
+import pyrado
+import torch as to
+from pyrado.algorithms.meta.sprl import SPRL
+from pyrado.algorithms.step_based.gae import GAE
+from pyrado.algorithms.step_based.ppo import PPO
+from pyrado.domain_randomization.domain_parameter import SelfPacedLearnerParameter
+from pyrado.domain_randomization.domain_randomizer import DomainRandomizer
+from pyrado.environment_wrappers.action_normalization import ActNormWrapper
+from pyrado.environment_wrappers.domain_randomization import DomainRandWrapperLive
+from pyrado.environments.pysim.quanser_qube import QQubeSwingUpSim
+from pyrado.logger.experiment import setup_experiment, save_dicts_to_yaml
+from pyrado.policies.feed_forward.fnn import FNNPolicy
+from pyrado.spaces import ValueFunctionSpace
+from pyrado.utils.argparser import get_argparser
+from pyrado.utils.data_types import EnvSpec
+from torch.optim import lr_scheduler
+
+if __name__ == "__main__":
+    # Parse command line arguments
+    args = get_argparser().parse_args()
+
+    # Experiment (set seed before creating the modules)
+    ex_dir = setup_experiment(QQubeSwingUpSim.name, f"{PPO.name}_{FNNPolicy.name}", f"100Hz_seed_{args.seed}")
+
+    # Set seed if desired
+    pyrado.set_seed(args.seed, verbose=True)
+
+    # Environment
+    env_hparams = dict(dt=1 / 100.0, max_steps=600)
+    env = QQubeSwingUpSim(**env_hparams)
+    env = ActNormWrapper(env)
+
+    # Policy
+    policy_hparam = dict(hidden_sizes=[64, 64], hidden_nonlin=to.tanh)  # FNN
+    # policy_hparam = dict(hidden_size=32, num_recurrent_layers=1)  # LSTM & GRU
+    policy = FNNPolicy(spec=env.spec, **policy_hparam)
+    # policy = GRUPolicy(spec=env.spec, **policy_hparam)
+
+    # Critic
+    vfcn_hparam = dict(hidden_sizes=[32, 32], hidden_nonlin=to.relu)  # FNN
+    # vfcn_hparam = dict(hidden_size=32, num_recurrent_layers=1)  # LSTM & GRU
+    vfcn = FNNPolicy(spec=EnvSpec(env.obs_space, ValueFunctionSpace), **vfcn_hparam)
+    # vfcn = GRUPolicy(spec=EnvSpec(env.obs_space, ValueFunctionSpace), **vfcn_hparam)
+    critic_hparam = dict(
+        gamma=0.9844224855479998,
+        lamda=0.9700148505302241,
+        num_epoch=5,
+        batch_size=500,
+        standardize_adv=False,
+        lr=7.058326426522811e-4,
+        max_grad_norm=6.0,
+        lr_scheduler=lr_scheduler.ExponentialLR,
+        lr_scheduler_hparam=dict(gamma=0.999),
+    )
+    critic = GAE(vfcn, **critic_hparam)
+
+    # Subroutine
+    algo_hparam = dict(
+        max_iter=1,
+        eps_clip=0.12648736789309026,
+        min_steps=30 * env.max_steps,
+        num_epoch=7,
+        batch_size=500,
+        std_init=0.7573286998997557,
+        lr=6.999956625305722e-04,
+        max_grad_norm=1.0,
+        num_workers=8,
+        lr_scheduler=lr_scheduler.ExponentialLR,
+        lr_scheduler_hparam=dict(gamma=0.999),
+    )
+    env = DomainRandWrapperLive(
+        env,
+        randomizer=DomainRandomizer(
+            SelfPacedLearnerParameter(
+                name="Rm",
+                target_mean=to.tensor([8.4]),
+                target_cov_chol_flat=to.tensor([1.0]),
+                context_mean=to.tensor([8.4]),
+                context_cov_chol_flat=to.tensor([0.1]),
+            ),
+            SelfPacedLearnerParameter(
+                name="Mr",
+                target_mean=to.tensor([0.095]),
+                target_cov_chol_flat=to.tensor([0.03]),
+                context_mean=to.tensor([0.095]),
+                context_cov_chol_flat=to.tensor([0.009]),
+            ),
+            SelfPacedLearnerParameter(
+                name="Mp",
+                target_mean=to.tensor([0.024]),
+                target_cov_chol_flat=to.tensor([0.01]),
+                context_mean=to.tensor([0.024]),
+                context_cov_chol_flat=to.tensor([0.005]),
+            ),
+        ),
+    )
+
+    sprl_hparam = dict(
+        kl_constraints_ub=1000,
+        performance_lower_bound=20,
+        std_lower_bound=0.002,
+        kl_threshold=0.1,
+        max_iter=100,
+    )
+    algo = SPRL(env, PPO(ex_dir, env, policy, critic, **algo_hparam), **sprl_hparam)
+
+    # Save the hyper-parameters
+    save_dicts_to_yaml(
+        dict(env=env_hparams, seed=args.seed),
+        dict(policy=policy_hparam),
+        dict(critic=critic_hparam, vfcn=vfcn_hparam),
+        dict(subrtn=algo_hparam, subrtn_name=PPO.name),
+        dict(algo=sprl_hparam, algo_name=algo.name),
+        save_dir=ex_dir,
+    )
+
+    # Jeeeha
+    algo.train(snapshot_mode="latest", seed=args.seed)
