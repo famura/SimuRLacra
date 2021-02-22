f05dae8ce0825c0d8aac5e5e50aa6fd49218711e
rmenzenbach <robin@menzenbachs.de>
2021-02-22 14:09:45 +0000

    Deploying to gh-pages from  @ 8d2a6ebd6884e0a07cec8cd5eb13fbd89d5b081c 🚀

--- rcspysim/search/functions_a.js
+++ rcspysim/search/functions_a.js
@@ -3,7 +3,8 @@ var searchData=
   ['main',['main',['../ECSRobo_8cpp.html#a3c04138a5bfe5d72780bb7e82a18e627',1,'main(int argc, char **argv):&#160;ECSRobo.cpp'],['../TestBotSim_8cpp.html#a3c04138a5bfe5d72780bb7e82a18e627',1,'main(int argc, char **argv):&#160;TestBotSim.cpp'],['../TestTargetTracking_8cpp.html#a3c04138a5bfe5d72780bb7e82a18e627',1,'main(int argc, char **argv):&#160;TestTargetTracking.cpp']]],
   ['markchanged',['markChanged',['../structRcs_1_1BodyParamInfo.html#a405a0ece221428445e2ca90626f2b62d',1,'Rcs::BodyParamInfo']]],
   ['matnd_5fcheckshapeeq',['MatNd_checkShapeEq',['../MatNd__based_8cpp.html#a56dc04907f5b74191217c582f4d5a44a',1,'MatNd_checkShapeEq(MatNd *m1, MatNd *m2):&#160;MatNd_based.cpp'],['../MatNd__based_8h.html#a56dc04907f5b74191217c582f4d5a44a',1,'MatNd_checkShapeEq(MatNd *m1, MatNd *m2):&#160;MatNd_based.cpp']]],
-  ['matnd_5fclipeleself',['MatNd_clipEleSelf',['../namespaceRcs.html#a76adf1eba11f5c329ae1400bf468316f',1,'Rcs']]],
+  ['matnd_5ffabsclipeleself',['MatNd_fabsClipEleSelf',['../namespaceRcs.html#aacac45efa2c8a06cf4bd7f81e73a54da',1,'Rcs']]],
+  ['matnd_5fsigns',['MatNd_signs',['../namespaceRcs.html#a729ce82995b65a0c8a0b5f131942ae27',1,'Rcs']]],
   ['matnd_5ftanh_5finplace',['MatNd_tanh_inplace',['../namespaceRcs.html#aeb80bb2dd8f83e476d5465a88fdc3e3c',1,'Rcs']]],
   ['mlplayer',['MLPLayer',['../structRcs_1_1MLPPolicy_1_1MLPLayer.html#a61907e660d1e663f482f6790f2349848',1,'Rcs::MLPPolicy::MLPLayer']]],
   ['mlppolicy',['MLPPolicy',['../classRcs_1_1MLPPolicy.html#aa74ad02eb90c1d64b86401f17de5d7bf',1,'Rcs::MLPPolicy']]]
