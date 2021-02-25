84911817b825d465904b7d6ee570df2e8d4ac6b8
Fabian Damken <fabian.damken@frisp.org>
2021-02-25 16:34:23 +0100

    Added QQ SU CEM.

---
+++ Pyrado/scripts/training/qq-su_cem.py
@@ -0,0 +1,85 @@
+# Copyright (c) 2020, Fabio Muratore, Honda Research Institute Europe GmbH, and
+# Technical University of Darmstadt.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of Fabio Muratore, Honda Research Institute Europe GmbH,
+#    or Technical University of Darmstadt, nor the names of its contributors may
+#    be used to endorse or promote products derived from this software without
+#    specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL FABIO MURATORE, HONDA RESEARCH INSTITUTE EUROPE GMBH,
+# OR TECHNICAL UNIVERSITY OF DARMSTADT BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+"""
+Train an agent to solve the Ball-on-Beam environment using Cross-Entropy Method .
+"""
+import pyrado
+from pyrado.algorithms.episodic.cem import CEM
+from pyrado.environment_wrappers.action_normalization import ActNormWrapper
+from pyrado.environments.pysim.quanser_cartpole import QCartPoleSwingUpSim
+from pyrado.environments.pysim.quanser_qube import QQubeSwingUpSim
+from pyrado.logger.experiment import setup_experiment, save_dicts_to_yaml
+from pyrado.policies.special.environment_specific import QCartPoleSwingUpAndBalanceCtrl, QQubeSwingUpAndBalanceCtrl
+from pyrado.utils.argparser import get_argparser
+
+
+if __name__ == "__main__":
+    # Parse command line arguments
+    args = get_argparser().parse_args()
+
+    # Experiment (set seed before creating the modules)
+    ex_dir = setup_experiment(QQubeSwingUpSim.name, f"{CEM.name}_{QQubeSwingUpAndBalanceCtrl.name}")
+
+    # Set seed if desired
+    pyrado.set_seed(args.seed, verbose=True)
+
+    # Environment
+    env_hparams = dict(dt=1 / 250.0, max_steps=12 * 250)
+    env = QQubeSwingUpSim(**env_hparams)
+
+    # Policy
+    policy_hparam = dict()
+    policy = QQubeSwingUpAndBalanceCtrl(env.spec, **policy_hparam)
+
+    # Algorithm
+    algo_hparam = dict(
+        max_iter=50,
+        pop_size=200,
+        num_init_states_per_domain=8,
+        num_is_samples=20,
+        expl_std_init=0.4,
+        expl_std_min=1e-2,
+        extra_expl_std_init=0.1,
+        extra_expl_decay_iter=30,
+        full_cov=False,
+        symm_sampling=False,
+        num_workers=10,
+    )
+    algo = CEM(ex_dir, env, policy, **algo_hparam)
+
+    # Save the hyper-parameters
+    save_dicts_to_yaml(
+        dict(env=env_hparams, seed=args.seed),
+        dict(policy=policy_hparam),
+        dict(algo=algo_hparam, algo_name=algo.name),
+        save_dir=ex_dir,
+    )
+
+    # Jeeeha
+    algo.train(seed=args.seed, snapshot_mode="best")
