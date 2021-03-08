7b85a6be21b870fcec0aea0e5756398adf2b881b
Fabian Damken <fabian.damken@stud.tu-darmstadt.de>
2021-03-08 12:27:34 +0100

    Incorporated more of Fabios review.

--- Pyrado/pyrado/utils/argparser.py
+++ Pyrado/pyrado/utils/argparser.py
@@ -235,7 +235,7 @@ def get_argparser() -> argparse.ArgumentParser:
         "-p",
         "--show_hparams",
         action="append",
-        help="The hyperparameters to show in the ask-for-experiment dialog. Use this parameter multiple times to show multiple hyperparameters.",
+        help="hyperparameters to show in the ask-for-experiment dialog; use this parameter multiple times to show multiple hyperparameters; e.g. to differentiate between experiments",
     )
 
     return parser
