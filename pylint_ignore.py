88b9f680fa2306ef880ea8857b1721f7f0f0b6a3
Fabian Damken <fabian.damken@stud.tu-darmstadt.de>
2021-04-22 20:42:18 +0200

    PyLintRC: Ignore scripts and tests.

---
+++ pylint_ignore.py
@@ -0,0 +1,17 @@
+""" Gratefully copied from https://github.com/PyCQA/pylint/issues/2686#issuecomment-621927895. """
+
+from pylint.utils import utils
+
+
+class PylintIgnorePaths:  # pylint: disable=too-few-public-methods
+    """ Helper class for ignoring directories in PyLint. """
+
+    def __init__(self, *paths):
+        self.paths = paths
+        self.original_expand_modules = utils.expand_modules
+        utils.expand_modules = self.patched_expand
+
+    def patched_expand(self, *args, **kwargs):  # pylint: disable=missing-function-docstring
+        result, errors = self.original_expand_modules(*args, **kwargs)
+        result = list(filter(lambda item: not any(True for path in self.paths if path in item["path"]), result))
+        return result, errors
