74e09740f4dd077cfed1dbc6629be2cb5868e014
Robin Menzenbach <robin@menzenbachs.de>
2021-05-31 17:44:41 +0200

    Remove high_memory flag; Minor fixes (#66)

--- Pyrado/tests/algorithms/test_meta.py
+++ Pyrado/tests/algorithms/test_meta.py
@@ -382,7 +382,6 @@ def test_sysidasrl_reps(ex_dir, env: SimEnv, num_eval_rollouts: int):
 
 
 @pytest.mark.longtime
-@pytest.mark.high_memory
 @pytest.mark.parametrize("env", ["default_qqsu"], ids=["qqsu"], indirect=True)
 def test_simopt_cem_ppo(ex_dir, env: SimEnv):
     pyrado.set_seed(0)
@@ -479,7 +478,6 @@ def test_simopt_cem_ppo(ex_dir, env: SimEnv):
     assert algo.curr_iter == algo.max_iter
 
 
-@pytest.mark.high_memory
 @pytest.mark.parametrize("env", ["default_qbb"], ids=["qbb"], indirect=True)
 @pytest.mark.parametrize(
     "policy",
@@ -550,7 +548,6 @@ def test_basic_meta(ex_dir, policy, env: SimEnv, algo, algo_hparam: dict):
     assert algo.curr_iter == algo.max_iter
 
 
-@pytest.mark.high_memory
 @pytest.mark.longtime
 @pytest.mark.parametrize("env", ["default_qqsu"], ids=["qqsu"], indirect=True)
 @pytest.mark.parametrize(
@@ -673,7 +670,6 @@ def test_npdr_no_policy_optimization(
     assert algo.curr_iter == algo.max_iter
 
 
-@pytest.mark.high_memory
 @pytest.mark.longtime
 @pytest.mark.parametrize("env", ["default_qqsu"], indirect=True)
 @pytest.mark.parametrize("optimize_mean", [False, True])
