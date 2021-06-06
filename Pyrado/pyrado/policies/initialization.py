419978440a8e4521d5a0bcb5b35967f666e38bf7
Heiko Carrasco <heiko.carrasco@yahoo.com>
2021-06-06 18:16:57 +0200

    Adapt nn init to the one in pytorch 1.8

--- Pyrado/pyrado/policies/initialization.py
+++ Pyrado/pyrado/policies/initialization.py
@@ -66,56 +66,64 @@ def init_param(m, **kwargs):
         else:
             init.normal_(m.data)
 
-    elif isinstance(m, (nn.Linear, nn.RNN, nn.GRU, nn.GRUCell)):
+    elif isinstance(m, nn.Linear):
         for name, param in m.named_parameters():
             if "weight" in name:
                 if param.ndim >= 2:
                     # Most common case
-                    init.orthogonal_(param.data)  # former: init.xavier_normal_(param.data)
+                    init.kaiming_uniform_(param.data, a=sqrt(5))
                 else:
                     init.normal_(param.data)
             elif "bias" in name:
-                if kwargs.get("uniform_bias", False):
-                    init.uniform_(param.data, a=-1.0 / sqrt(param.data.nelement()), b=1.0 / sqrt(param.data.nelement()))
-                else:
-                    # Default case
-                    init.normal_(param.data, std=1.0 / sqrt(param.data.nelement()))
+                fan_in, _ = init._calculate_fan_in_and_fan_out(param.data)
+                bound = 1 / sqrt(fan_in) if fan_in > 0 else 0
+                init.uniform_(param.data, -bound, bound)
             else:
                 raise pyrado.KeyErr(keys="weight or bias", container=param)
 
+    elif isinstance(m, (nn.RNN, nn.GRU, nn.GRUCell)):
+        stdv = 1.0 / sqrt(m.hidden_size)
+        for name, param in m.named_parameters():
+            init.uniform_(param, -stdv, stdv)
+
     elif isinstance(m, (nn.LSTM, nn.LSTMCell)):
         for name, param in m.named_parameters():
-            if "weight_ih" in name:
-                # Initialize the input to hidden weights orthogonally
-                # w_ii, w_if, w_ic, w_io
-                nn.init.orthogonal_(param.data)
-            elif "weight_hh" in name:
-                # Initialize the hidden to hidden weights separately as identity matrices and stack them afterwards
-                # w_ii, w_if, w_ic, w_io
-                weight_hh_ii = to.eye(m.hidden_size, m.hidden_size)
-                weight_hh_if = to.eye(m.hidden_size, m.hidden_size)
-                weight_hh_ic = to.eye(m.hidden_size, m.hidden_size)
-                weight_hh_io = to.eye(m.hidden_size, m.hidden_size)
-                weight_hh_all = to.cat([weight_hh_ii, weight_hh_if, weight_hh_ic, weight_hh_io], dim=0)
-                param.data.copy_(weight_hh_all)
-            elif "bias" in name:
-                # b_ii, b_if, b_ig, b_io
-                if "t_max" in kwargs:
-                    assert isinstance(kwargs["t_max"], (float, int, to.Tensor)), pyrado.TypeErr(
-                        given=kwargs["t_max"], expected_type=[float, int, to.Tensor]
-                    )
-                    # Initialize all biases to 0, but the bias of the forget and input gate using the chrono init
-                    nn.init.constant_(param.data, val=0)
-                    param.data[m.hidden_size : m.hidden_size * 2] = to.log(
-                        nn.init.uniform_(  # forget gate
-                            param.data[m.hidden_size : m.hidden_size * 2], 1, kwargs["t_max"] - 1
+            if "t_max" in kwargs:
+                if "weight_ih" in name:
+                    # Initialize the input to hidden weights orthogonally
+                    # w_ii, w_if, w_ic, w_io
+                    nn.init.orthogonal_(param.data)
+                elif "weight_hh" in name:
+                    # Initialize the hidden to hidden weights separately as identity matrices and stack them afterwards
+                    # w_ii, w_if, w_ic, w_io
+                    weight_hh_ii = to.eye(m.hidden_size, m.hidden_size)
+                    weight_hh_if = to.eye(m.hidden_size, m.hidden_size)
+                    weight_hh_ic = to.eye(m.hidden_size, m.hidden_size)
+                    weight_hh_io = to.eye(m.hidden_size, m.hidden_size)
+                    weight_hh_all = to.cat([weight_hh_ii, weight_hh_if, weight_hh_ic, weight_hh_io], dim=0)
+                    param.data.copy_(weight_hh_all)
+                elif "bias" in name:
+                    # b_ii, b_if, b_ig, b_io
+                    if "t_max" in kwargs:
+                        assert isinstance(kwargs["t_max"], (float, int, to.Tensor)), pyrado.TypeErr(
+                            given=kwargs["t_max"], expected_type=[float, int, to.Tensor]
                         )
-                    )
-                    param.data[0 : m.hidden_size] = -param.data[m.hidden_size : 2 * m.hidden_size]  # input gate
-                else:
-                    # Initialize all biases to 0, but the bias of the forget gate to 1
-                    nn.init.constant_(param.data, val=0)
-                    param.data[m.hidden_size : m.hidden_size * 2].fill_(1)
+                        # Initialize all biases to 0, but the bias of the forget and input gate using the chrono init
+                        nn.init.constant_(param.data, val=0)
+                        param.data[m.hidden_size : m.hidden_size * 2] = to.log(
+                            nn.init.uniform_(  # forget gate
+                                param.data[m.hidden_size : m.hidden_size * 2], 1, kwargs["t_max"] - 1
+                            )
+                        )
+                        param.data[0 : m.hidden_size] = -param.data[m.hidden_size : 2 * m.hidden_size]  # input gate
+                    else:
+                        # Initialize all biases to 0, but the bias of the forget gate to 1
+                        nn.init.constant_(param.data, val=0)
+                        param.data[m.hidden_size : m.hidden_size * 2].fill_(1)
+            else:
+                stdv = 1.0 / sqrt(m.hidden_size)
+                for name, param in m.named_parameters():
+                    init.uniform_(param, -stdv, stdv)
 
     elif isinstance(m, nn.Conv1d):
         if kwargs.get("bell", False):
