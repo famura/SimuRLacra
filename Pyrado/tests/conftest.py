8cc4bb357c0e9cbdf91c6297e170c4e34f254cbf
Fabio Muratore <fabio.muratore@famura.net>
2021-06-22 09:01:06 +0200

    Fixed mujoco import from PR #74

--- Pyrado/tests/conftest.py
+++ Pyrado/tests/conftest.py
@@ -41,7 +41,6 @@ from pyrado.domain_randomization.domain_parameter import (
 )
 from pyrado.domain_randomization.domain_randomizer import DomainRandomizer
 from pyrado.environments.base import Env
-from pyrado.environments.mujoco.quanser_qube import QQubeStabMjSim, QQubeSwingUpMjSim
 from pyrado.environments.one_step.catapult import CatapultSim
 from pyrado.environments.one_step.rosenbrock import RosenSim
 from pyrado.environments.pysim.ball_on_beam import BallOnBeamDiscSim, BallOnBeamSim
@@ -114,6 +113,7 @@ try:
     from pyrado.environments.mujoco.openai_half_cheetah import HalfCheetahSim
     from pyrado.environments.mujoco.openai_hopper import HopperSim
     from pyrado.environments.mujoco.wam_bic import WAMBallInCupSim
+    from pyrado.environments.mujoco.quanser_qube import QQubeStabMjSim, QQubeSwingUpMjSim
 
     m_needs_mujoco = pytest.mark.skipif(False, reason="mujoco-py can be imported.")
 
