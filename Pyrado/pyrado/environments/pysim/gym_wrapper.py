4a1c90f3e99960f2cd2de0434d0e2f75a5dd2967
famura <37794142+famura@users.noreply.github.com>
2021-08-03 09:39:39 +0200

    Update gym_wrapper.py

--- Pyrado/pyrado/environments/pysim/gym_wrapper.py
+++ Pyrado/pyrado/environments/pysim/gym_wrapper.py
@@ -1,3 +1,31 @@
+# Copyright (c) 2020, Fabio Muratore, Honda Research Institute Europe GmbH, and
+# Technical University of Darmstadt.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of Fabio Muratore, Honda Research Institute Europe GmbH,
+#    or Technical University of Darmstadt, nor the names of its contributors may
+#    be used to endorse or promote products derived from this software without
+#    specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL FABIO MURATORE, HONDA RESEARCH INSTITUTE EUROPE GMBH,
+# OR TECHNICAL UNIVERSITY OF DARMSTADT BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 import gym
 import gym.spaces
 import numpy as np
@@ -11,63 +39,62 @@ from pyrado.utils.data_types import RenderMode
 
 
 class PysimGymWrapper(gym.Env):
-    """A wrapper for pysim environments exposing a gym env.
-    Do not instantiate this yourself but rather use `gym.make()` like this::
+    """
+    A wrapper for pysim environments exposing a gym env.
+    Do not instantiate this yourself but rather use `gym.make()` like this
+    
+    .. code-block:: python
 
         sim_env = QQubeSwingUpSim(**env_hparams)
         gym.make("SimulacraPySimEnv-v0", env=sim_env)
     """
 
-    metadata = {"render.modes": ["human"]}
-    """Supported render modes (currently only human)
-    """
+    metadata = {"render.modes": ["human"]} # supported render modes: currently only human
 
     def __init__(self, env: SimPyEnv):
-        """Initialize the environment.
-        You are not supposed to call this function directly but use `gym.make("SimulacraPySimEnv-v0", env=sim_env)`
-        where `sim_env` is your Pyrado environment.
+        """
+        Initialize the environment. You are not supposed to call this function directly, but use
+        `gym.make("SimulacraPySimEnv-v0", env=sim_env)` where `sim_env` is your Pyrado environment.
 
-        :param env: Wrapped Pyrado environment
+        :param env: wrapped Pyrado environment
         """
         self._wrapped_env = env
 
-        # translate environment parameters
+        # Translate environment parameters
         self.action_space = self.conv_space_from_simulacra(self._wrapped_env.act_space)
         self.observation_space = self.conv_space_from_simulacra(self._wrapped_env.obs_space)
 
-    def step(self, action):
-        """Run one timestep of the environment's dynamics. When end of
-        episode is reached, you are responsible for calling `reset()`
-        to reset this environment's state.
-        Accepts an action and returns a tuple (observation, reward, done, info).
-        Args:
-            action (object): an action provided by the agent
-        Returns:
-            observation (object): agent's observation of the current environment
-            reward (float) : amount of reward returned after previous action
-            done (bool): whether the episode has ended, in which case further step() calls will return undefined results
-            info (dict): contains auxiliary diagnostic information (helpful for debugging, and sometimes learning)
+    def step(self, act: np.ndarray) -> Tuple[np.ndarray, float, bool, dict]:
         """
-        return self._wrapped_env.step(action)
+        Run one timestep of the environment's dynamics. When end of episode is reached, you are responsible for calling 
+        `reset()` to reset this environment's state.
+        
+        :param act: action provided by the agent
+        :return: observation: agent's current observation of the environment
+                 reward: reward returned by the environment
+                 done: whether the episode has ended, in which case further `step()` calls will return undefined results
+                 info: contains auxiliary diagnostic information (helpful for debugging, and sometimes learning)
+        """
+        return self._wrapped_env.step(act)
 
-    def reset(self):
-        """Resets the environment to an initial state and returns an initial
-        observation.
-        Note that this function does not reset the environment's random
-        number generator(s); random variables in the environment's state are
-        sampled independently between multiple calls to `reset()`. In other
-        words, each call of `reset()` yields an environment suitable for
-        a new episode, independent of previous episodes.
+    def reset(self) -> np.ndarray:
+        """
+        Resets the environment to an initial state and returns an initial observation.
+        
+        .. note::
+            This function does not reset the environment's random number generator(s); random variables in the 
+            environment's state are sampled independently between multiple calls to `reset()`. In other words, each call 
+            of `reset()` yields an environment suitable for a new episode, independent of previous episodes.
 
-        :returns observation: the initial observation.
+        :return: the initial observation
         """
         return self._wrapped_env.reset()
 
-    def render(self, mode="human"):
-        """Renders the environment.
-        The set of supported modes varies per environment. (And some
-        environments do not support rendering at all.) By convention,
-        if mode is:
+    def render(self, mode: str="human"):
+        """
+        Renders the environment.
+        The set of supported modes varies per environment (some environments do not support rendering).
+        By convention, if mode is:
         - human: render to the current display or terminal and
           return nothing. Usually for human consumption.
         - rgb_array: Return an numpy.ndarray with shape (x, y, 3),
@@ -79,33 +106,31 @@ class PysimGymWrapper(gym.Env):
 
         :param mode: the mode to render with (currently ignored until other modes besides human are implemented)
         """
-
         self._wrapped_env.render(mode=RenderMode(text=False, video=True, render=False), render_step=1)
 
     def close(self):
-        """Perform any necessary cleanup for the environment.
-        Environments will automatically `close()` themselves when
+        """
+        Perform any necessary cleanup for the environment. Environments will automatically `close()` themselves when
         garbage collected or when the program exits.
         """
         self._wrapped_env.close()
 
-    def seed(self, seed=None):
-        """Sets the seed for this env's random number generator(s).
+    def seed(self, seed: Optional[int]=None) -> Optional[List[int]]:
+        """
+        Sets the seed for this env's random number generator(s).
 
-        :return: list: Returns the list of seeds used in this env's random
-              number generators. The first value in the list should be the
-              "main" seed, or the value which a reproducer should pass to
-              'seed'. Often, the main seed equals the provided 'seed', but
-              this won't be true if seed=None, for example.
+        :return: list of seeds used in this environments's random number generators. The first value in the list 
+                 should be the "main" seed, or the value which a reproducer should pass to `seed`. Often, the main 
+                 seed equals the provided `seed`, but this won't be true if `seed=None`, for example.
         """
         pyrado.set_seed(seed)
 
     @staticmethod
     def conv_space_from_simulacra(space: Space) -> gym.spaces.Space:
-        """Convert a Simulacra space to a gym space
+        """
+        Convert a Simulacra space to a gym space
 
         :param space: A simulacra space
-        :raises NotImplementedError: Raised when no conversion is implemented for the space type
         :return:
         """
         if isinstance(space, BoxSpace):
