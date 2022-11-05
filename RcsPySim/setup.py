d324cca06f5ca58d0c4351128ca5172a1c9d8921
famura <fabio.muratore@famura.net>
2022-11-05 14:11:53 +0100

    Ran black formatter

--- RcsPySim/setup.py
+++ RcsPySim/setup.py
@@ -73,7 +73,7 @@ class CMakeBuild(build_ext):
 
             if platform.system() == "Windows":
                 cmake_args += ["-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)]
-                if sys.maxsize > 2 ** 32:
+                if sys.maxsize > 2**32:
                     cmake_args += ["-A", "x64"]
                 build_args += ["--", "/m"]
             else:
